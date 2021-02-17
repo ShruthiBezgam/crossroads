f7c2c062b767c0ecbf9211d07d6d2f6e309d1773
unknown <shruthibezgam2@gmail.com>
2021-02-17 16:40:05 -0500

    Added UI, component code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crossroads';
+
+  rows = [
+    {
+      "CommitID" : "1",
+      "Author" : "Rahul",
+      "Time" : "3:00 AM",
+      "CommitMessage" : "Commit 1"
+    },
+    {
+      "CommitID" : "2",
+      "Author" : "Ritu",
+      "Time" : "2:45 PM",
+      "CommitMessage" : "Commit 2"
+    },
+    {
+      "CommitID" : "3",
+      "Author" : "Raghu",
+      "Time" : "12:25 PM",
+      "CommitMessage" : "Commit 3"
+    }]
 }
