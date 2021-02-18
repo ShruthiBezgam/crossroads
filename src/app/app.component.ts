691a0b79bac57768b5966f94204ff50dbf0a8777
ShruthiBezgam <shruthib@cvemail.com>
2021-02-18 14:46:40 -0500

    Final Commit with basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,60 +11,34 @@ export class AppComponent {
   httpResponse: any;
   display: any;
   resultArray: any;
-  dataArray: any;
+  dataArray = [];
   errorResp: any;
 
   constructor(
-    public service: BackendService){}
-  
-    ngOnInit(): void {
-     this.gitHistoryServiceCall();
-    }
+    public service: BackendService) { }
 
-  gitHistoryServiceCall(){
+  ngOnInit(): void {
+    this.gitHistoryServiceCall();
+  }
+
+  gitHistoryServiceCall() {
     this.service.gitCommitHistoryService().subscribe(Resp => {
       this.httpResponse = Resp;
-      this.display = JSON.stringify(this.httpResponse);
+      
       console.log(" git response : ", Resp)
-      if (this.httpResponse.errorCode == 0) {
-        console.log("this.httpResponse.msg   -->", this.httpResponse.msg)
-        
-        this.resultArray = this.httpResponse.result;
-        
-        for (var i = 0; i < this.resultArray; i++) {
-
-          var data = {
-            CommitID: this.resultArray[i].sha,
-            Author: this.resultArray[i].name,
-            Time: this.resultArray[i].date,
-            CommitMessage: this.resultArray[i].message
-          }
-          this.dataArray.push(data);
+      var data = {}
+      this.resultArray = this.httpResponse;
+      console.log("resultArray[0]", this.resultArray[0].sha)
+      for (var i = 0; i < this.resultArray.length; i++) {
+        data = {
+          "CommitID": this.resultArray[i].sha,
+          "Author": this.resultArray[i].commit.author.name,
+          "Time": this.resultArray[i].commit.author.date,
+          "CommitMessage": this.resultArray[i].commit.message
         }
-
-      } else {
-        this.errorResp = this.httpResponse.msg
+        this.dataArray.push(data);
       }
-    })
-}
 
-  // rows = [
-  //   {
-  //     "CommitID" : "1",
-  //     "Author" : "Rahul",
-  //     "Time" : "3:00 AM",
-  //     "CommitMessage" : "Commit 1"
-  //   },
-  //   {
-  //     "CommitID" : "2",
-  //     "Author" : "Ritu",
-  //     "Time" : "2:45 PM",
-  //     "CommitMessage" : "Commit 2"
-  //   },
-  //   {
-  //     "CommitID" : "3",
-  //     "Author" : "Raghu",
-  //     "Time" : "12:25 PM",
-  //     "CommitMessage" : "Commit 3"
-  //   }]
+    })
+  } 
 }
