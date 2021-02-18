ae9c4e3718ad1224fd4ca1f3da16118c3fe073ce
ShruthiBezgam <shruthib@cvemail.com>
2021-02-17 20:37:50 -0500

    Added API in the backend service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BackendService } from './backend-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +8,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crossroads';
+  httpResponse: any;
+  display: any;
+  resultArray: any;
+  dataArray: any;
+  errorResp: any;
 
-  rows = [
-    {
-      "CommitID" : "1",
-      "Author" : "Rahul",
-      "Time" : "3:00 AM",
-      "CommitMessage" : "Commit 1"
-    },
-    {
-      "CommitID" : "2",
-      "Author" : "Ritu",
-      "Time" : "2:45 PM",
-      "CommitMessage" : "Commit 2"
-    },
-    {
-      "CommitID" : "3",
-      "Author" : "Raghu",
-      "Time" : "12:25 PM",
-      "CommitMessage" : "Commit 3"
-    }]
+  constructor(
+    public service: BackendService){}
+  
+    ngOnInit(): void {
+     this.gitHistoryServiceCall();
+    }
+
+  gitHistoryServiceCall(){
+    this.service.gitCommitHistoryService().subscribe(Resp => {
+      this.httpResponse = Resp;
+      this.display = JSON.stringify(this.httpResponse);
+      console.log(" git response : ", Resp)
+      if (this.httpResponse.errorCode == 0) {
+        console.log("this.httpResponse.msg   -->", this.httpResponse.msg)
+        
+        this.resultArray = this.httpResponse.result;
+        
+        for (var i = 0; i < this.resultArray; i++) {
+
+          var data = {
+            CommitID: this.resultArray[i].sha,
+            Author: this.resultArray[i].name,
+            Time: this.resultArray[i].date,
+            CommitMessage: this.resultArray[i].message
+          }
+          this.dataArray.push(data);
+        }
+
+      } else {
+        this.errorResp = this.httpResponse.msg
+      }
+    })
+}
+
+  // rows = [
+  //   {
+  //     "CommitID" : "1",
+  //     "Author" : "Rahul",
+  //     "Time" : "3:00 AM",
+  //     "CommitMessage" : "Commit 1"
+  //   },
+  //   {
+  //     "CommitID" : "2",
+  //     "Author" : "Ritu",
+  //     "Time" : "2:45 PM",
+  //     "CommitMessage" : "Commit 2"
+  //   },
+  //   {
+  //     "CommitID" : "3",
+  //     "Author" : "Raghu",
+  //     "Time" : "12:25 PM",
+  //     "CommitMessage" : "Commit 3"
+  //   }]
 }
--- src/app/backend-service.service.ts
+++ src/app/backend-service.service.ts
@@ -1,9 +1,27 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BackendServiceService {
+export class BackendService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  /**
+   * calling backend git Commit History service
+   */
+  gitCommitHistoryService(){
+    console.log('Calling Authentication service ');
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',    
+      }),
+    };
+    return this.http.get(
+      environment.Endpoint,
+      httpOptions,
+    );
+  }
 }
